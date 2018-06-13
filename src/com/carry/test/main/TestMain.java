870dc9688c5d925da9317e7ea350bc2b01658670
liuzhi@zidingjr.com <liuzhi123>
2018-06-13 10:03:35 +0800

    初始导入

---
+++ src/com/carry/test/main/TestMain.java
@@ -0,0 +1,20 @@
+package com.carry.test.main;
+
+/**
+ *
+ * @author Carry.Liu
+ * @createDate 2018Äê6ÔÂ13ÈÕ
+ */
+public class TestMain {
+
+	public static void main(String[] args) {
+		
+		/*====================================*/
+		
+		System.out.println("Hello world£¡");
+		
+		/*====================================*/
+		
+	}
+	
+}
